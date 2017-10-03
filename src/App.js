54a406d9c945a4808d2003642ef6ab345a63d112
TristanKwant <tristankwant@gmail.com>
2017-10-03 10:05:18 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from './styles/theme'
+import Navigation from './components/ui/Navigation'
+import Routes from './routes'
+import './App.css'
+
+class App extends Component {
+  static childContextTypes = {
+    muiTheme: PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    return { muiTheme }
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="App">
+          <Navigation />
+          <Routes />
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default App
