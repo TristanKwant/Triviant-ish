6891f937168e1bbaa55dbeae468e2e7a7c953169
jobweeda <jobweeda@hotmail.com>
2017-10-03 10:51:21 +0200

    adds testquestions

---
+++ src/fixtures/questions.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    _id: 1
+    question: ["Where am I?", "Here"],
+    answers:  ["There", "Where", "Here", "I dont know"]
+  },
+  {
+    _id: 2
+    question: ["Ananas or ananas?", "Pineapple"]
+    answers:  ["Ananas", "ananas", "Potato", "Pineapple"]
+  },
+
+]
