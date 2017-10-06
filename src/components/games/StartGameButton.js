30a8a90d3373ad36ce42366591a270833ce1c8bf
jobweeda <jobweeda@hotmail.com>
2017-10-06 15:13:37 +0200

    startbutton

---
+++ src/components/games/StartGameButton.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import RaisedButton from 'material-ui/RaisedButton'
+import StarIcon from 'material-ui/svg-icons/action/favorite'
+import startGame from '../../actions/games/create'
+
+class StartGameButton extends PureComponent {
+  static propTypes = {
+    signedIn: PropTypes.bool,
+  }
+
+  render() {
+    if (!this.props.signedIn) return null
+
+    return (
+      <div className="CreateGameButton">
+        <RaisedButton
+          label="START"
+          primary={true}
+          onClick={this.props.startGame}
+          icon={<StarIcon />} />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ currentUser }) => ({
+  signedIn: !!currentUser && !!currentUser._id,
+})
+
+export default connect(mapStateToProps, { startGame })(StartGameButton)
