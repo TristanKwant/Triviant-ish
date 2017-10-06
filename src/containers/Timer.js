50a0e7c75e0214fd40b115ca4090b07f80a7baad
faisalalsudani <f4desiign@gmail.com>
2017-10-06 10:26:46 +0200

    not to merge

--- src/containers/Timer.js
+++ src/containers/Timer.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import timerAction from '../actions/timerAction'
 import addNr from '../actions/questionNr/addNr'
 import LinearProgress from 'material-ui/LinearProgress';
+import Game from './Game'
+import subscribeToGames from '../actions/games/subscribe'
 import './Timer.css'
 
 
@@ -10,46 +12,31 @@ class Timer extends PureComponent {
 
   constructor() {
     super();
-    // this.state = { time: {}, seconds: 10 };
-    // this.timer = 10;
     this.countDown = this.countDown.bind(this);
   }
 
-
-
   componentDidMount() {
-    this.props.timerAction(100)
+    this.props.timerAction(10)
     this.startTimer()
   }
 
-
   startTimer(){
-// debugger
-    // this.setState({ time: this.props.timer });
     if (this.props.timer === 10) {
       var interVal = setInterval(this.countDown, 1000);
-    this.setState({interVal: interVal});
-
+      this.setState({interVal: interVal});
     }
-
   }
 
+
   countDown() {
-
-
     let seconds = this.props.timer -1;
     this.props.timerAction(seconds)
-    // this.setState({
-    //   time: seconds,
-    //   seconds: seconds,
-    // });
+
     if (this.props.timer < 0) {
-      // this.props.timer()
       clearInterval(this.state.interVal);
       this.props.timerAction(10)
       this.startTimer()
       this.props.addNr(this.props.questionsNr +1)
-
     }
   }
 
@@ -59,14 +46,16 @@ class Timer extends PureComponent {
 
   render() {
     return (
-
       <div className="timer-bar">
         <span>Time left to answer: {this.props.timer}</span>
         <LinearProgress mode="determinate" value={this.props.timer} max={10} className={'timer'} />
+
       </div>
     )
   }
 }
+
+
 
 const mapStateToProps = ({ timer, questionsNr }) => ({
   timer, questionsNr
